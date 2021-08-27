b9f00ac862d2cd25d3703a68645b85cc41de4dcb
worodhazam <78118439+worodhazam@users.noreply.github.com>
2021-08-27 10:34:41 +0400

    Add files via upload

---
+++ application.php
@@ -0,0 +1,181 @@
+<div class="w">
+  <h1>
+Swincar Applications
+<br>
+
+</h1>
+<article>
+  <p data-aos="fade-left">
+
+The extreme versatility
+The versatility of Swincar e-Spider and its driving controls means it is suitable for:
+
+Leisure activities
+Agricultural use
+Civil and military applications
+People with disabilities
+Options are available for specific requirements such as the transfer of people with reduced mobility.
+
+Different uses and applications
+The Swincar e-Spider is of course a leisure vehicle suitable for land, trails, riding in the mountains, in forests or on beaches by rediscover nature while respecting it.
+
+System Operation
+The nacelle and the wheels are articulated on longitudinal axes allowing tilting when you turn and correction of the angle on slopes.
+
+To produce the pivot in the right direction (towards the inside of the turn or uphill), it is necessary that:
+
+The pivoting axis of the platform is located above the center of gravity.
+Pivot axis of each wheel passes under the contact point of the wheel with the ground.
+The nacelle on one side and the wheels on the other side, benefits independently to each other, from a pendular equilibrium and could theoretically not be associated in their pivoting movement.
+
+"The concept of pendulum tilt is protected by international patents"
+
+Pendulum principle
+The word pendulum is often wrongly used to describe to a tilting vehicle.
+
+A "Pendulum based Vehicle" is a vehicle that tilt positions in balance when it applies the force of gravity, the centrifugal force and the ground reaction. There is therefore no activator or electronic management.
+
+The operation is passive and not active so it is lively and reliable. Vehicles with an active tilt activator all faced the problem of the response speed of the activator within acceptable power consumption. In an emergency situation a delay in movement of the actuator could be dangerous.
+  </p>
+</article>
+</div>
+<style type="text/css">
+  @import url('https://fonts.googleapis.com/css?family=Exo+2:700');
+* {
+  margin: 0 auto;
+  padding: 0;
+}
+
+.w {
+
+  background-color: #151414;
+  background-blend-mode: overlay;
+  background-position: center;
+  background-size: cover;
+  text-align: center;
+  height: 700px;
+  padding-top: 100px;
+}
+
+.w h1 {
+  color: whitesmoke;
+  font-family: 'Exo 2', sans-serif;
+  font-size: 46px;
+  font-weight: 900;
+  text-transform: uppercase;
+
+}
+
+.w p {
+  max-width: 700px;
+  min-height: 200px;
+  padding: 0;
+  color: whitesmoke;
+  font-family: OpenSans;
+  font-size: 14px;
+  font-weight: 300;
+  line-height: 22px;
+}
+
+.square {
+  height: 60px;
+  width: 60px;
+  border: 1px dashed white;
+  margin: 0 0 0 55px;
+  /*   padding: 1px; resize squares */
+  background-color: rgba(255, 255, 255, 0.2);
+  display: inline-block;
+  transform: rotateZ(44deg);
+}
+
+.square:hover {
+  background-color: rgba(27, 182, 239, 0.2);
+  transition: ease 0.2s;
+  cursor: pointer;
+}
+
+.square .icons {
+  position: absolute;
+  transform: rotateZ(-44deg);
+  margin: 20px 0 0px 21px;
+}
+
+.fa-facebook,
+.fa-twitter,
+.fa-dribbbler,
+.fa-youtube,
+.fa-dribbble {
+  width: 11px;
+  height: 22px;
+  color: white;
+  font-family: FontAwesome;
+  font-size: 23px;
+  font-weight: 400;
+  text-transform: uppercase;
+}
+
+.square:hover .fa-facebook {
+  color: rgba(59, 89, 152, 1)
+}
+
+.square:hover .fa-twitter {
+  color: rgba(27, 182, 239, 1)
+}
+
+.square:hover .fa-dribbble {
+  color: rgba(199, 59, 111, 1)
+}
+
+.square:hover .fa-youtube {
+  color: rgba(229, 45, 39, 1)
+}
+.logo{
+   margin: 1em;
+   position: fixed;
+   bottom: -72px;
+   z-index: 99999999999;
+   right: 0;
+}
+svg{
+
+ 
+
+}
+.pen{
+  fill:black;
+ animation:rotateInDownLeft 3s forwards;
+}
+.all{
+  animation:rotateOut 3s forwards;
+}
+@keyframes rotateInDownLeft {
+  from {
+    transform-origin: left bottom;
+    transform: rotate3d(0,0,0, 0deg);
+    opacity: 1;
+  }
+
+  to {
+    -webkit-transform-origin: left bottom;
+    transform-origin: left bottom;
+ transform: ;
+ transform:translateX(850px) translateY(-83px) rotate3d(0,0,1, -60deg);
+    opacity: 1;
+  }
+}
+@keyframes rotateOut {
+  from {
+    -webkit-transform-origin: center;
+    transform-origin: center;
+    opacity: 1;
+  }
+
+  to {
+    -webkit-transform-origin: center;
+    transform-origin: center;
+    -webkit-transform: rotate3d(0, 0, 1, 90deg);
+    transform: rotate3d(0, 0, 1, 90deg);
+    opacity: 1;
+  }
+}
+</style>
